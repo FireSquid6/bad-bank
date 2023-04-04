eebb0d9520bc59f79c0b5d76eb6b21400b220bc9
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2023-04-03 22:33:44 -0500

    add header'

---
+++ src/header/Header.tsx
@@ -0,0 +1,15 @@
+
+export default function Header() {
+
+  return (
+    <header className="header">
+      <h1>Bad Bank</h1>
+      <nav>
+        <a>Create Account</a>
+        <a>Withdraw</a>
+        <a>Deposit</a>
+        <a>All Data</a>
+      </nav>
+    </header>
+  )
+}
