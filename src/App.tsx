bc697770aa5d7d10e59ee2c545a3f2c768e91285
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2023-04-05 20:29:26 -0500

    add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import CreateAccount from './pages/create-account/CreateAccount'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
         <Header />
         <Routes>
