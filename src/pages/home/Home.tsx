0cf8424cee9bbe4ee55362c69af17ee983b7441d
firesquid <jdeiss06@gmail.com>
2023-04-17 17:17:45 -0500

    fix router in production

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="home-page"> 
       <h1 role="title">Welcome to the bad bank!</h1>
       <p role="description">This is an insecure fake banking website</p>
-      <img role="image"/>
+      <img role="image" src="panama-bank-account.jpg"/>
     </main>
   )
 }
