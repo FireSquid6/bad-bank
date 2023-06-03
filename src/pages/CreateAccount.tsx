c9e06949638baaf0cfa133f32468d4927b9fb227
firesquid06 <jdeiss06@gmail.com>
2023-06-03 14:30:44 -0500

    add extra styling to authentication page

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,14 +1,13 @@
 import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { backendClient } from "../backend/client";
 
 export default function CreateAccount() {
   return (
-    <Auth
-      supabaseClient={backendClient}
-      appearance={{ theme: ThemeSupa }}
-      providers={[]}
-      dark={true}
-    />
+    <div className="w-full flex align-middle justify-center">
+      <div className="max-w-md text-white">
+        <h4 className="text-3xl m-3 text-center">Create Account / Sign In</h4>
+        <Auth supabaseClient={backendClient} providers={[]} />
+      </div>
+    </div>
   );
 }
