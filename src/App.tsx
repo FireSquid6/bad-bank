fa86da49b9b608ff72c64753aae097726553dbce
firesquid06 <jdeiss06@gmail.com>
2023-06-04 19:23:07 -0500

    add UI for deposit and withdraw pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import Header from "./header/Header";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateAccount from "./pages/CreateAccount";
+import Operation from "./pages/Operation";
 
 export default function App() {
   return (
-    <main role="main" className="bg-zinc-700 h-screen text-white">
+    <main role="main" className="bg-zinc-900 h-screen text-white">
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-account" element={<CreateAccount />} />
+          <Route
+            path="/deposit"
+            element={<Operation title="Deposit" input_multiplier={1} />}
+          />
+          <Route
+            path="/withdraw"
+            element={<Operation title="Withdraw" input_multiplier={-1} />}
+          />
         </Routes>
       </BrowserRouter>
     </main>
---
+++ src/pages/Operation.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export interface OperationProps {
+  title: string;
+  input_multiplier?: number;
+}
+
+export default function Operation({
+  title,
+  input_multiplier = 1,
+}: OperationProps) {
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <div className="w-full flex align-middle justify-center">
+      <div className="max-w-md text-white">
+        <h4 className="text-3xl m-3 text-center">{title}</h4>
+        <div className="flex flex-col m-2">
+          <label className="text-lg">Amount</label>
+          <input
+            className="text-white bg-zinc-800 p-1 outline-none"
+            type="number"
+            step="1"
+            onChange={(e) => {
+              const value = e.target.value;
+              setAmount(parseInt(value) || 0);
+            }}
+            defaultValue={0}
+          />
+        </div>
+        <div className="w-full flex flex-row align-middle justify-center">
+          <button
+            className="bg-green-500 text-white w-36 h-12"
+            onSubmit={() => {
+              console.log(amount * input_multiplier);
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
